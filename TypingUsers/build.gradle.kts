feadad0fbc83892a43a55dbad54f3f09ac59d931
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-03-26 11:44:34 -0700

    feat(TypingUsers): bump version + changelog

--- TypingUsers/build.gradle.kts
+++ TypingUsers/build.gradle.kts
@@ -1,12 +1,16 @@
-version = "1.0.0"
-description = "Opens a clickable list of all the currently typing users by pressing on the bar."
+version = "2.0.0"
+description = "Shows all the currently typing users by long clicking the typing users bar."
 
 aliucord {
-	excludeFromUpdaterJson.set(true)
 	changelog.set(
 		"""
 			# 1.0.0
 			* Released
+
+			# 2.0.0
+			* Use a separate screen instead of BottomSheet
+			* Show currently and previously typing users
+			* Fix FragmentManager crash
 		""".trimIndent()
 	)
 }
